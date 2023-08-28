875f721feabc9ddcad154b2ce00c3ac5b64b8bcc
Daniyal Ashraf Khan <dani.ashraf@outlook.com>
2023-08-28 15:44:40 +0500

    Added plan to follow to develop the calculator

---
+++ index.js
@@ -0,0 +1,51 @@
+// Plan
+// 1. Add basic functions:
+//   - add
+//   - subtract
+//   - multiply
+//   - divide
+
+
+
+// 2. Variables (3) in calc ops:
+//   - number
+//   - operator
+//   - another number
+
+// 3. function operate():
+//   - parameters (operator, num1, num2)
+//   - calls above basic function to calculate
+
+// 4. HTML:
+//   - basic html
+//   - buttons for each digit
+//   - buttons for each function
+//   - button for equal sign
+//   - display for calc
+//   - button for "clear"
+
+// 5. display function:
+//   - function that updates the display
+//   - display value stored in variable for use in next step
+
+// 6. make calc work:
+//   - store first and second number as inputs
+//   - utilize operator that user selects
+//   - operate() on two nums when user presses "=" key
+//   - operate() should update display with solution
+//   - figure out how to store all values and call operate() with them
+
+// 7. potential bugs to fix:
+//   - should be able to handle multiple numbers and operators
+//     as well as bodmas rules
+//   - evaluate first pair of numbers then continue rather than all at once
+//   - round answers with long decimals so they dont overflow screen
+//   - pressing "=" before entering all nums/operator could cause problems
+//   - pressing "clear" should wipe existing data, user starts fresh
+//   - display message if user tries to divide by 0, dont let calc crash
+
+// 8. Extra creds:
+//   - allow users to add floating point numbers but make sure only one decimal
+//   - make it look nice, sexy css, bootstrap etc
+//   - add "backspace" buttons
+//   - add keyboard support
