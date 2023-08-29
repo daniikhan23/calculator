0ddd0f3eb460d1078e81021b4de89b906a1428c6
Daniyal Ashraf Khan <dani.ashraf@outlook.com>
2023-08-29 15:11:58 +0500

    Added event listeners to all number buttons and now calculations can be performed with them

--- index.js
+++ index.js
@@ -57,12 +57,77 @@ let firstNum;
 let secondNum;
 let displayArray = [];
 
+const numZero = document.querySelector(".btn-zero");
+numZero.addEventListener("click", () => {
+  displayArray.push("0");
+  displayResult.textContent += "0";
+  console.log(displayResult.textContent);
+});
+
 const numOne = document.querySelector(".btn-one");
 numOne.addEventListener("click", () => {
   displayArray.push("1");
   displayResult.textContent += "1";
   console.log(displayResult.textContent);
 });
+
+const numTwo = document.querySelector(".btn-two");
+numTwo.addEventListener("click", () => {
+  displayArray.push("2");
+  displayResult.textContent += "2";
+  console.log(displayResult.textContent);
+});
+
+const numThree = document.querySelector(".btn-three");
+numThree.addEventListener("click", () => {
+  displayArray.push("3");
+  displayResult.textContent += "3";
+  console.log(displayResult.textContent);
+});
+
+const numFour = document.querySelector(".btn-four");
+numFour.addEventListener("click", () => {
+  displayArray.push("4");
+  displayResult.textContent += "4";
+  console.log(displayResult.textContent);
+});
+
+const numFive = document.querySelector(".btn-five");
+numFive.addEventListener("click", () => {
+  displayArray.push("5");
+  displayResult.textContent += "5";
+  console.log(displayResult.textContent);
+});
+
+const numSix = document.querySelector(".btn-six");
+numSix.addEventListener("click", () => {
+  displayArray.push("6");
+  displayResult.textContent += "6";
+  console.log(displayResult.textContent);
+});
+
+const numSeven = document.querySelector(".btn-seven");
+numSeven.addEventListener("click", () => {
+  displayArray.push("7");
+  displayResult.textContent += "7";
+  console.log(displayResult.textContent);
+});
+
+const numEight = document.querySelector(".btn-eight");
+numEight.addEventListener("click", () => {
+  displayArray.push("8");
+  displayResult.textContent += "8";
+  console.log(displayResult.textContent);
+});
+
+const numNine = document.querySelector(".btn-nine");
+numNine.addEventListener("click", () => {
+  displayArray.push("9");
+  displayResult.textContent += "9";
+  console.log(displayResult.textContent);
+});
+
+
 
 // function operate():
 
